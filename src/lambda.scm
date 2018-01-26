bea1fa307fae76835f6a72ab3f00fe26a4703ee6
hennin <hennin.ltn@gmail.com>
2018-01-27 05:36:29 +0900

    [add]Church Boolean

---
+++ src/lambda.scm
@@ -0,0 +1,57 @@
+;; Utilities
+(define-syntax print-exp
+  (syntax-rules ()
+    ((_ exp) (print (string-append "input  = "
+                                   (format "~s" 'exp)
+                                   "\noutput = "
+                                   (format "~s" exp))))))
+
+(define-syntax reduct
+  (syntax-rules ()
+    ((_ f1 f2 f3 ...) (reduct (f1 f2) f3 ...))
+    ((_ f1 f2) (f1 f2))
+    ((_ f) f)))
+
+;; Church Boolean
+(define tru
+  (lambda (t)
+    (lambda (f)
+      t)))
+
+
+(define fls
+  (lambda (t)
+    (lambda (f)
+      f)))
+
+(define test
+  (lambda (l)
+    (lambda (m)
+      (lambda (n)
+        (reduct l m n)))))
+
+(define _and
+  (lambda (b)
+    (lambda (c)
+      (reduct b c fls))))
+
+(define _or
+  (lambda (b)
+    (lambda (c)
+      (reduct b tru c))))
+
+(define not
+  (lambda (b)
+    (reduct b fls tru)))
+
+;;; e.g.
+
+(print-exp (reduct test tru 'v 'w))
+(print-exp (reduct test fls 'v 'w))
+(print-exp (reduct _and tru tru 'v 'w))
+(print-exp (reduct _and tru fls 'v 'w))
+(print-exp (reduct _or fls tru 'v 'w))
+(print-exp (reduct _or fls fls 'v 'w))
+(print-exp (reduct not tru 'v 'w))
+(print-exp (reduct not fls 'v 'w))
+
