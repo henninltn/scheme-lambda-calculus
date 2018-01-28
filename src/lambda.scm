48d363abafddfb50cd0fc3b5cd85850522fca926
hennin <hennin.ltn@gmail.com>
2018-01-29 02:12:48 +0900

    [add]List

--- src/lambda.scm
+++ src/lambda.scm
@@ -8,9 +8,10 @@
 
 (define-syntax reduct
   (syntax-rules ()
-    ((_ f1 f2 f3 ...) (reduct (f1 f2) f3 ...))
-    ((_ f1 f2) (f1 f2))
+    ((_ f a1 a2 ...) (reduct (f a1) a2 ...))
+    ((_ f a) (f1 a))
     ((_ f) f)))
+
 
 ;; Church Boolean
 (define tru
@@ -163,4 +164,70 @@
 (print-exp (reduct iszero (reduct subtract c2 c1) 'v 'w))
 (print-exp (reduct equal c3 c3 'v 'w))
 (print-exp (reduct equal c3 c1 'v 'w))
+
+
+;; List (foldr)
+(define nil
+  (lambda (c)
+    (lambda (n)
+      n)))
+
+(define cons
+  (lambda (h)
+    (lambda (t)
+      (lambda (c)
+        (lambda (n)
+          (reduct c h (reduct t c n)))))))
+
+(define isnil
+  (lambda (l)
+    (reduct l (lambda (h) (lambda (t) fls)) tru)))
+
+(define head
+  (lambda (l)
+    (reduct l tru fls)))
+
+(define tail
+  (lambda (l)
+    (fst (reduct l
+                 (lambda (x)
+                   (lambda (p)
+                     (reduct pair (snd p) (reduct (reduct cons x (snd p))))))
+                 (reduct pair nil nil)))))
+
+(define nil_s
+  (reduct pair tru tru))
+
+(define cons_s
+  (lambda (h)
+    (lambda (t)
+      (reduct pair fls (reduct pair h t)))))
+
+(define head_s
+  (lambda (z)
+    (fst (snd z))))
+
+(define tail_s
+  (lambda (z)
+    (snd (snd z))))
+
+(define isnil_s
+  fst)
+
+;;; e.g.
+(print-exp (reduct nil 'v 'w))
+(print-exp (reduct isnil nil 'v 'w))
+(print-exp (reduct isnil (reduct cons 'w nil) 'v 'w))
+(print-exp (head (reduct cons 'v (reduct cons 'w nil))))
+(print-exp (head (tail (reduct cons 'v (reduct cons 'w nil)))))
+(print-exp (reduct (tail (tail (reduct cons 'v (reduct cons 'w nil)))) 'v 'w))
+
+(print-exp nil_s)
+(print-exp (reduct fst nil_s 'v 'w))
+(print-exp (reduct snd nil_s 'v 'w))
+(print-exp (reduct isnil_s nil_s))
+(print-exp (reduct isnil_s (reduct cons_s 'w nil_s)))
+(print-exp (head_s (reduct cons_s 'v (reduct cons_s 'w nil_s))))
+(print-exp (head_s (tail_s (reduct cons_s 'v (reduct cons_s 'w nil_s)))))
+(print-exp (tail_s (tail_s (reduct cons_s 'v (reduct cons_s 'w nil_s)))))
 
